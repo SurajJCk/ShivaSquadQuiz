c975fc8e9072439cde9c7995704a6773d3bca117
Suraj Jyoti Changkakoti <sjckashyap97@gmail.com>
2025-02-18 15:47:31 +0530

    Fix: Update deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
